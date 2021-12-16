3df8bbbbfe032e206c227efaea13483c63c2f90a
Dabrit <ly9460730472580800@163.com>
2021-12-16 14:46:38 +0800

    none

--- main.py
+++ main.py
@@ -5,35 +5,39 @@ from bilibili_api import live, sync
 from os import popen
 
 
-def room(roomnum):
-    return live.LiveDanmaku(roomnum)
 
 class Asoul(object):
-    def __init__(self,room,uid):
-        self.room = room
+    def __init__(self,roomnum,uid):
+        self.roomnum
+        self.uid
+    def  getbullet(roomnun):
+        room = live.LiveDanmaku(roomnun)
+
+        @room.on('DANMU_MSG')
+        async def on_danmaku(event):
+            # 收到弹幕
+            print(event)
+
+        sync(room.connect())
+
+
 class Diana(Asoul):
-    self.Diana.room = room(22637261)
-    self.Ava.room = room(22625025)
-    self.Queen.room = room(22625027)
-    self.Kira.room = room(22632424)
-    self.Carol.room = room(22634198)
+    Asoul.roomnum = 22637261
+    Asoul.uid  = 672328094
+class Ava(Asoul):
+    Asoul.roomnum = 22625025
+    Asoul.uid = 672346917
+class Queen(Asoul):
+    Asoul.roomnum = 22625027
+    Asoul.uid = 672342685
 
+class Kira(Asoul):
+    Asoul.roomnum = 22632424
+    Asoul.uid = 672353429
+class Carol(Asoul):
+    Asoul.roomnum = 22634198
+    Asoul.uid = 351609538
 
-UID = {
-
-       'Diana':672328094,
-
-       'Ava':672346917,
-
-       'Queen':672342685,
-
-       'Kira':672353429,
-
-       'Carol':351609538
-
-}
-
-roomlist = [Asoul.Diana.room,Avaroom,Queenroom,Kiraroom,Carolroom]
 
 def main():
     # detect the liveroom pushstream statues
@@ -46,15 +50,7 @@ def main():
         # judge the status:
         # json analysis replace by T:
         if True:
-            room =
-
-            @room.on('DANMU_MSG')
-            async def on_danmaku(event):
-                # 收到弹幕
-                print(event)
-
-            sync(room.connect())
-            # ing
+            Diana.getbullet()
 
 
 if __name__=='__main__':
