517b4a226cae7c9f573cd8f4ddbbe6327d795980
Dabrit <ly9460730472580800@163.com>
2022-01-14 23:57:29 +0800

    20%

--- db_process.py
+++ db_process.py
@@ -1,12 +1,36 @@
+import toml
 import pymongo
 
-class liveinfo_db(object):
-    def __init__(self,username,content,repeat_count):
-        db_client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
-        temp_danmu_db = db_client["liveinfo"]
+authinfo = toml.load("./db_auth.toml")["userme"]
+db_client: MongoClient = pymongo.MongoClient("mongodb://127.0.0.1:27017/",
+                                             username=authinfo["username"],
+                                             password=authinfo["password"],
+                                             authSource=authinfo["authSource"],
+                                             authMechanism=authinfo["authMechanism"])
+
+
+class liveinfo_process(object):
+
+    def __init__(self, username, content, plague, uid, repeat_count):
+        pass
+
+    #        dbuser = urllib.parse.quote_plus(str(input("username of local db: ")))
+    #        dbpasswd = urllib.parse.quote_plus(str(input("password: ")))
+    def check_database(self):
+
         if "liveinfo" in db_client.list_database_names():
-            pass
+            print("Database has exist, check collection: ")
+            if "danmuinfo" in db_client.list_collection_names():
+                print("collection has exist")
+
         else:
-            pass
+            temp_danmu_db = db_client["liveinfo"]
+            temp_danmu_col = temp_danmu_db["danmuinfo"]
 
-
+    def insert(self, username, content, plague, uid, repeat_count):
+        formatodict = {"uid": f"{uid}",
+                       "username": f"{username}",
+                       "content": f"{content}",
+                       "plague": f"{plague}",
+                       "repeat_count": f"{repeat_count}"}
+        db_client["liveinfo"]["danmuinfo"].insert_one(formatodict)
--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from bilibili_api import live, sync
 import db_process
 
 
-
 class Asoul(object):
-    def __init__(self,roomnum,uid):
+    def __init__(self, roomnum, uid):
         self.roomnum = roomnum
         self.uid = uid
 
-
-    def get_livestatus(self,uid):
+    def get_livestatus(self, uid):
         url = 'https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids'
         header = {'Content-Type': 'application/json'}
         middle = {'uids': [uid]}
@@ -23,46 +21,45 @@ class Asoul(object):
         if statusvalue == 2:
             return True
 
-
-    def  get_bullet(self,roomnun):
+    def get_bullet(self, roomnun):
         room = live.LiveDanmaku(roomnun)
 
         @room.on('DANMU_MSG')
-
-
         async def on_danmaku(event):
             # 收到弹幕
             # process the dic
-            def recursive_get_value(data,key_to_find):
-               if key_to_find in data:
+            def recursive_get_value(data, key_to_find):
+                if key_to_find in data:
                     return data[key_to_find]
 
-               for v in data.values():
-                   if isinstance(v, dict):
-                       return recursive_get_value(v, key_to_find)
-               return None
+                for v in data.values():
+                    if isinstance(v, dict):
+                        return recursive_get_value(v, key_to_find)
+                return None
 
-            username = recursive_get_value(event,'info')[2][1]
-            content = json.loads(recursive_get_value(event,'info')[0][15]['extra'])["content"]
-            uid = recursive_get_value(event,'info')[2][0]
+            username = recursive_get_value(event, 'info')[2][1]
+            content = json.loads(recursive_get_value(event, 'info')[0][15]['extra'])["content"]
+            uid = recursive_get_value(event, 'info')[2][0]
             try:
                 plaque = f"{recursive_get_value(event, 'info')[3][1]}" \
                          f"{recursive_get_value(event, 'info')[3][0]}"
             except IndexError:
                 plaque = None
 
-
             print(f"{uid} {plaque} {username}:  {content}")
+
         sync(room.connect())
 
-Diana = Asoul(22637261,672328094)
-Ava   = Asoul(22625025,672346917)
-Queen = Asoul(22625027,672342685)
-Kira  = Asoul(22632424,672353429)
-Carol = Asoul(22634198,351609538)
+
+Diana = Asoul(22637261, 672328094)
+Ava = Asoul(22625025, 672346917)
+Queen = Asoul(22625027, 672342685)
+Kira = Asoul(22632424, 672353429)
+Carol = Asoul(22634198, 351609538)
+
 
 def main():
-    Members = [Diana,Ava,Queen,Kira,Carol]
+    Members = [Diana, Ava, Queen, Kira, Carol]
     for member in Members:
         judge = member.get_livestatus(member.uid)
         while judge is True:
@@ -70,12 +67,5 @@ def main():
             member.get_bullet(member.roomnum)
 
 
-
-
-
-
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
